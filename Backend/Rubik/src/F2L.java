1b5c531a3c402d87bf499327db20aa1e6da15b26
ChefChilliSauce <navshukla30@gmail.com>
2024-11-12 17:15:56 +0530

    F2L

--- Backend/Rubik/src/F2L.java
+++ Backend/Rubik/src/F2L.java
@@ -1,2 +1,11 @@
 public class F2L {
+    final private char[][][] cube;
+    final private CubeMoves cubeMoves;
+
+    public F2L(RubiksCube rubiksCube) {             //getter method
+        // Directly assign the cube array to a member variable
+        this.cube = rubiksCube.getCube();
+        // Initialize CubeMoves with the RubiksCube instance
+        this.cubeMoves = new CubeMoves(rubiksCube);
+    }
 }
