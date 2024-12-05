9668dcbf65db9e51c482b6555b8acaace50be42f
ChefChilliSauce <navshukla30@gmail.com>
2024-12-05 18:31:14 +0530

    Tested new f r ba l moves

--- Backend/Rubik/src/Main.java
+++ Backend/Rubik/src/Main.java
@@ -2,14 +2,38 @@ public class Main {
     public static void main(String[] args) {
         RubiksCube cube = new RubiksCube();
         CubeMoves moves = new CubeMoves(cube);
-        moves.rotateMiddleLeftRightClockwise();
-        moves.rotateMiddleLeftRightClockwise();
-        moves.rotateMiddleLeftRightClockwise();
-        moves.rotateMiddleLeftRightClockwise();
-        moves.rotateMiddleLeftRightCounterClockwise();
-        moves.rotateMiddleLeftRightCounterClockwise();
-        moves.rotateMiddleLeftRightCounterClockwise();
-        moves.rotateMiddleLeftRightCounterClockwise();
+        moves.rotateDoubleFrontClockwise();
+        moves.rotateDoubleFrontClockwise();
+        moves.rotateDoubleFrontClockwise();
+        moves.rotateDoubleFrontClockwise();
+        moves.rotateDoubleFrontCounterClockwise();
+        moves.rotateDoubleFrontCounterClockwise();
+        moves.rotateDoubleFrontCounterClockwise();
+        moves.rotateDoubleFrontCounterClockwise();
+        moves.rotateDoubleBackClockwise();
+        moves.rotateDoubleBackClockwise();
+        moves.rotateDoubleBackClockwise();
+        moves.rotateDoubleBackClockwise();
+        moves.rotateDoubleBackCounterClockwise();
+        moves.rotateDoubleBackCounterClockwise();
+        moves.rotateDoubleBackCounterClockwise();
+        moves.rotateDoubleBackCounterClockwise();
+        moves.rotateDoubleRightClockwise();
+        moves.rotateDoubleRightClockwise();
+        moves.rotateDoubleRightClockwise();
+        moves.rotateDoubleRightClockwise();
+        moves.rotateDoubleRightCounterClockwise();
+        moves.rotateDoubleRightCounterClockwise();
+        moves.rotateDoubleRightCounterClockwise();
+        moves.rotateDoubleRightCounterClockwise();
+        moves.rotateDoubleLeftClockwise();
+        moves.rotateDoubleLeftClockwise();
+        moves.rotateDoubleLeftClockwise();
+        moves.rotateDoubleLeftClockwise();
+        moves.rotateDoubleLeftCounterClockwise();
+        moves.rotateDoubleLeftCounterClockwise();
+        moves.rotateDoubleLeftCounterClockwise();
+        moves.rotateDoubleLeftCounterClockwise();
         cube.DisplayCube();
     }
 }
