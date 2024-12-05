49ef65f322caf411d3788d4703341855077cf330
ChefChilliSauce <navshukla30@gmail.com>
2024-12-05 17:52:37 +0530

    Tested new MID RL and LR moves

--- Backend/Rubik/src/Main.java
+++ Backend/Rubik/src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args) {
         RubiksCube cube = new RubiksCube();
         CubeMoves moves = new CubeMoves(cube);
+        moves.rotateMiddleLeftRightClockwise();
+        moves.rotateMiddleLeftRightClockwise();
+        moves.rotateMiddleLeftRightClockwise();
+        moves.rotateMiddleLeftRightClockwise();
+        moves.rotateMiddleLeftRightCounterClockwise();
+        moves.rotateMiddleLeftRightCounterClockwise();
+        moves.rotateMiddleLeftRightCounterClockwise();
+        moves.rotateMiddleLeftRightCounterClockwise();
         cube.DisplayCube();
     }
 }
