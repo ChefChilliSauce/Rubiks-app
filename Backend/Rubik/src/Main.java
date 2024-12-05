0622d5a10f43b722c4a45754869a5b1f2892b7db
ChefChilliSauce <navshukla30@gmail.com>
2024-12-05 17:26:16 +0530

    Tested new moves

--- Backend/Rubik/src/Main.java
+++ Backend/Rubik/src/Main.java
@@ -2,19 +2,6 @@ public class Main {
     public static void main(String[] args) {
         RubiksCube cube = new RubiksCube();
         CubeMoves moves = new CubeMoves(cube);
-
-/*
-        moves.rotateTopClockwise();
-        moves.rotateRightClockwise();
-        moves.rotateLeftClockwise();
-        moves.rotateFrontCounterClockwise();
-        moves.rotateRightCounterClockwise();
-        moves.rotateTopCounterClockwise();
-
- */
-
-
-
         cube.DisplayCube();
     }
 }
