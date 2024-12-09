4e4f953658b99a28d900ce86d82b8767ba0b0f4d
Chilli Sauce <navshukla30@gmail.com>
2024-12-09 13:05:35 +0530

    Error

--- Backend/Rubik/src/Main.java
+++ Backend/Rubik/src/Main.java
@@ -1,39 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        RubiksCube rubiksCube = new RubiksCube();
         RubiksCube cube = new RubiksCube();
         CubeMoves moves = new CubeMoves(cube);
-        moves.rotateDoubleFrontClockwise();
-        moves.rotateDoubleFrontClockwise();
-        moves.rotateDoubleFrontClockwise();
-        moves.rotateDoubleFrontClockwise();
-        moves.rotateDoubleFrontCounterClockwise();
-        moves.rotateDoubleFrontCounterClockwise();
-        moves.rotateDoubleFrontCounterClockwise();
-        moves.rotateDoubleFrontCounterClockwise();
-        moves.rotateDoubleBackClockwise();
-        moves.rotateDoubleBackClockwise();
-        moves.rotateDoubleBackClockwise();
-        moves.rotateDoubleBackClockwise();
-        moves.rotateDoubleBackCounterClockwise();
-        moves.rotateDoubleBackCounterClockwise();
-        moves.rotateDoubleBackCounterClockwise();
-        moves.rotateDoubleBackCounterClockwise();
-        moves.rotateDoubleRightClockwise();
-        moves.rotateDoubleRightClockwise();
-        moves.rotateDoubleRightClockwise();
-        moves.rotateDoubleRightClockwise();
-        moves.rotateDoubleRightCounterClockwise();
-        moves.rotateDoubleRightCounterClockwise();
-        moves.rotateDoubleRightCounterClockwise();
-        moves.rotateDoubleRightCounterClockwise();
-        moves.rotateDoubleLeftClockwise();
-        moves.rotateDoubleLeftClockwise();
-        moves.rotateDoubleLeftClockwise();
-        moves.rotateDoubleLeftClockwise();
-        moves.rotateDoubleLeftCounterClockwise();
-        moves.rotateDoubleLeftCounterClockwise();
-        moves.rotateDoubleLeftCounterClockwise();
-        moves.rotateDoubleLeftCounterClockwise();
+        CFOPSolver cfopSolver = new CFOPSolver(rubiksCube);
+        moves.rotateLeftCounterClockwise();
+        moves.rotateRightClockwise();
+        moves.rotateTopClockwise();
+        moves.rotateBottomCounterClockwise();
+        //moves.rotateBackClockwise();
+
+//       moves. rotateTopCounterClockwise();
+//        moves.rotateLeftClockwise();
+//        moves.rotateTopCounterClockwise();
+//      moves.rotateBottomCounterClockwise();
+//       moves.rotateFrontClockwise();
+//      moves.rotateBackClockwise();
+//        moves.rotateRightCounterClockwise();
+//        moves.rotateBottomCounterClockwise();
+//        moves.rotateTopClockwise();
+//        moves.rotateFrontClockwise();
+//        moves.rotateLeftCounterClockwise();
+
         cube.DisplayCube();
     }
 }
