0d9234ba52651fd4d7f14a971683e4a11f088cc9
Chilli Sauce <navshukla30@gmail.com>
2024-12-11 12:07:34 +0530

    Used perform moves from cube moves

--- Backend/Rubik/src/CFOPSolver.java
+++ Backend/Rubik/src/CFOPSolver.java
@@ -1,6 +1,6 @@
 public class CFOPSolver {
     private final CrossSolver crossSolver;
-    private final FLcorners firstLayerCorner;
+    //private final FLcorners firstLayerCorner;
     private final F2L f2lSolver;
     private final OLL ollSolver;
     private final PLL pllSolver;
@@ -8,7 +8,7 @@ public class CFOPSolver {
     public CFOPSolver(RubiksCube rubiksCube) {
         // Initialize solvers with the Rubik's Cube instance
         this.crossSolver = new CrossSolver(rubiksCube);
-        this.firstLayerCorner = new FLcorners(rubiksCube);
+        //this.firstLayerCorner = new FLcorners(rubiksCube);
         this.f2lSolver = new F2L(rubiksCube);
         this.ollSolver = new OLL(rubiksCube);
         this.pllSolver = new PLL(rubiksCube);
@@ -19,7 +19,6 @@ public class CFOPSolver {
         //System.out.println("Solving the White Cross...");
         crossSolver.solveWhiteCross();
         //System.out.println("White Cross Solved!");
-        firstLayerCorner.solveWhiteCorners();
         f2lSolver.solveSecondLayer();
       ollSolver.solveOLL();
         pllSolver.solvePLL();
