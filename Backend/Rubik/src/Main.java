085e3d2f710e37554b00f2c0a89b7a21ddf8dc9b
Chilli Sauce <navshukla30@gmail.com>
2024-12-12 06:44:03 +0530

    nothing

--- Backend/Rubik/src/Main.java
+++ Backend/Rubik/src/Main.java
@@ -1,16 +1,37 @@
 public class Main {
+
     public static void main(String[] args) {
         RubiksCube cube = new RubiksCube();
         CubeMoves moves = new CubeMoves(cube);
         CFOPSolver cfopSolver = new CFOPSolver(cube);
-       moves. rotateTopCounterClockwise();
-        moves.rotateLeftClockwise();
-        moves.rotateTopCounterClockwise();
-      moves.rotateBottomCounterClockwise();
-       moves.rotateFrontClockwise();
-     moves.rotateBackClockwise();
-        moves.rotateRightCounterClockwise();
-        moves.rotateBottomCounterClockwise();
+//        moves.rotateTopCounterClockwise();
+//        moves.rotateLeftClockwise();
+//        moves.rotateTopCounterClockwise();
+//        moves.rotateBottomCounterClockwise();
+//        moves.rotateFrontClockwise();
+//        moves.rotateBackClockwise();
+//        moves.rotateRightCounterClockwise();
+//        moves.rotateBottomCounterClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateTopCounterClockwise();
+//        moves.rotateBottomCounterClockwise();
+//        moves.rotateRightCounterClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateLeftCounterClockwise();
+//        moves.rotateFrontCounterClockwise();
+//        moves.rotateLeftCounterClockwise();
+//        moves.rotateRightCounterClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateBackClockwise();
+//        moves.rotateLeftCounterClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateBottomClockwise();
+//        moves.rotateTopCounterClockwise();
+//        moves.rotateTopCounterClockwise();
+//        moves.rotateLeftClockwise();
+//        moves.rotateLeftClockwise();
+//        moves.rotateBackCounterClockwise();
         cfopSolver.solveCube();
         cube.DisplayCube();
     }
